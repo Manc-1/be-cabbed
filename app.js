9a36d027b9b14ad931672d2171a0c45c785aa560
VoltrefferNL <nielsdevisser@hotmail.com>
2020-05-18 16:15:31 +0100

    Added connection to MongoDB

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
-const express = require('express')
-const app = express()
-const port = 9090
+const express = require("express");
+const mongoose = require("mongoose");
+
+const User = require("./model/user");
+require("dotenv/config");
+
+const app = express();
+const port = 9090;
 
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/", (req, res) => res.send("Hello World!"));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.post("/create_user", async (req, res) => {
+  try {
+    const myuser = new User(req.body);
+    await myuser.save();
+    res.send(myuser);
+  } catch (err) {
+    res.send("message: err");
+  }
+});
+
+mongoose.connect(
+  process.env.DB_Connection,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (req, res) => {
+    console.log("Connected to the database");
+  }
+);
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
 
 module.exports = app;
