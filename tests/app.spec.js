9649eb572f0fe771d30dae2f243f59cda812d906
joannakendall <58439846+joannakendall@users.noreply.github.com>
2020-05-19 15:23:20 +0100

    testing for a post get and delete user request

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -1,4 +1,4 @@
-const expect = require('chai');
+const {expect} = require('chai');
 const User = require('../model/user');
 
 describe('', () => {
@@ -11,6 +11,47 @@ describe('', () => {
             user.save().then(() => {
                 expect(user.isNew).to.equal(false)
             done();
+            })
+        })
+    })
+    describe('finding users', () => {
+        beforeEach((done) => {
+            const user = new User({
+                name:'joanna',
+                password:'password123'
+            })
+            user.save().then(() => {
+            done();
+            })
+        })
+        it('finds one user by name from the database', (done) => {
+            User.findOne({name: 'joanna'}).then((res) => {
+                expect(res.name).to.equal('joanna')
+                done();
+            })
+        })
+        it('finds one user by id from the database', (done) => {
+            User.findOne({_id: user._id}).then((res) => {
+                expect(res._id).to.eql(user._id)
+                done();
+            })
+        })
+    })
+    describe('delete user', () => {
+        beforeEach((done) => {
+            const user = new User({
+                name:'joanna',
+                password:'password123'
+            })
+            user.save().then(() => {
+            done();
+            })
+        })
+        it('deletes user from database', () => {
+            User.findOneAndRemove({name:'joanna'}).then(() => {
+                User.findOne({name:'joanna'}).then((res) => {
+                    expect(res).to.equal(null)
+                })
             })
         })
     })
