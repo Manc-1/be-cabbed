ad252b78c77c0db3a3e682f9e88224fe3d56e637
joannakendall <58439846+joannakendall@users.noreply.github.com>
2020-05-21 15:41:04 +0100

    changed route to pickupRouter

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -1,9 +1,9 @@
 const apiRouter = require("express").Router();
 
 const usersRouter = require("./usersRouter");
-const dataRouter = require("./dataRouter");
+const pickupRouter = require("./pickupRouter");
 
 apiRouter.use("/users", usersRouter);
-apiRouter.use("/data", dataRouter);
+apiRouter.use("/pickup", pickupRouter);
 
 module.exports = apiRouter;
