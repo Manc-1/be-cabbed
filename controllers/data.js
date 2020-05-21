db4c486462bf435c63d587d02309f5b81cc8c6c4
joannakendall <58439846+joannakendall@users.noreply.github.com>
2020-05-21 09:10:25 +0100

    requied in data

--- controllers/data.js
+++ controllers/data.js
@@ -1,6 +1,9 @@
-const User = require("../model/user.js");
+const Data = require("../model/data.js");
 require("dotenv/config");
 
 exports.sendData = () => {
     console.log('in controllers')
+    //const points = req.body.points
+    //const type = req.body.type
+    //Data.save(newData)
 }
