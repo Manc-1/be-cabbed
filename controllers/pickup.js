4e3c82a87ea1844b86bfd5ef693467ecb52111f5
joannakendall <58439846+joannakendall@users.noreply.github.com>
2020-05-22 11:28:14 +0100

    date and time split

--- controllers/pickup.js
+++ controllers/pickup.js
@@ -1,8 +1,8 @@
 const Pickup = require("../model/pickup.js");
 require("dotenv/config");
+const moment = require('moment');
 
 exports.sendPickup = (req, res, next) => {
-
     Pickup.find({}, function(err, pickup) {
         if (err) {
           res.send(err);
@@ -10,6 +10,20 @@ exports.sendPickup = (req, res, next) => {
           res.status(200).send({pickup});
         }
       })
+}
+
+exports.sendPickupFromHour = (req, res, next) => {
+  const myDate = new Date(Date.now())
+  const myDateStart = new Date(Date.now() - 1 * 60 * 60 * 1000)
+  const myTime = moment(myDate).format("h:mm:ss a")
+  const myTimeStart = moment(myDateStart).format("h:mm:ss a")
+  Pickup.find({time: { $gte : myTimeStart, $lte :  myTime }}, function(err, pickup) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.status(200).send({pickup});
+      }
+    })
 }
 
 exports.postPickup = async (req, res, next) => {
