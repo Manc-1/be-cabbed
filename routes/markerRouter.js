3fde917a72e072b36a7c656ecd76a1dc5e201d23
VoltrefferNL <nielsdevisser@hotmail.com>
2020-05-22 16:38:46 +0100

    Added 405 routes

--- routes/markerRouter.js
+++ routes/markerRouter.js
@@ -1,11 +1,14 @@
 const markerRouter = require("express").Router();
-const {postMarker, sendMarker, sendMarkerFromHour} = require('../controllers/marker')
+const {
+  postMarker,
+  sendMarker,
+  sendMarkerFromHour,
+} = require("../controllers/marker");
 
-markerRouter.route('')
-.get(sendMarker)
-.post(postMarker);
+const { send405Error } = require("../errors/index");
 
-markerRouter.route('/hour')
-.get(sendMarkerFromHour)
+markerRouter.route("").get(sendMarker).post(postMarker).all(send405Error);
+
+markerRouter.route("/hour").get(sendMarkerFromHour).all(send405Error);
 
 module.exports = markerRouter;
