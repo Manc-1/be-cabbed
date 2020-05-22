4440fd5eaf6f3007f0c2c17ffab752e8f4c25568
joannakendall <58439846+joannakendall@users.noreply.github.com>
2020-05-22 09:06:19 +0100

    added in a markerRouter

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -2,8 +2,10 @@ const apiRouter = require("express").Router();
 
 const usersRouter = require("./usersRouter");
 const pickupRouter = require("./pickupRouter");
+const markerRouter = require('./markerRouter');
 
 apiRouter.use("/users", usersRouter);
 apiRouter.use("/pickup", pickupRouter);
+apiRouter.use('/marker', markerRouter)
 
 module.exports = apiRouter;
