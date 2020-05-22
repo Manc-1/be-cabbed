6209e55b193c025e890d1e886c079b2a49770cfc
joannakendall <58439846+joannakendall@users.noreply.github.com>
2020-05-22 11:28:49 +0100

    new endpoint for getting data from previous hour

--- routes/pickupRouter.js
+++ routes/pickupRouter.js
@@ -1,8 +1,11 @@
 const pickupRouter = require("express").Router();
-const {sendPickup, postPickup} = require('../controllers/pickup')
+const {sendPickup, postPickup, sendPickupFromHour} = require('../controllers/pickup')
 
 pickupRouter.route('')
 .get(sendPickup)
 .post(postPickup);
+
+pickupRouter.route('/hour')
+.get(sendPickupFromHour);
 
 module.exports = pickupRouter;
