55f7d97672f97f584fbedf7a9d0c0a82eb6bff9a
danieljhark <danieljharkin@gmail.com>
2020-05-22 16:31:15 +0100

    added sendPicsendPickupFromPastHours to controllers

--- controllers/marker.js
+++ controllers/marker.js
@@ -1,43 +1,45 @@
 const Marker = require("../model/marker.js");
 require("dotenv/config");
-const moment = require('moment');
+const moment = require("moment");
 
 exports.sendMarker = (req, res, next) => {
-
-    Marker.find({}, function(err, marker) {
-        if (err) {
-          res.send(err);
-        } else {
-          res.status(200).send({marker});
-        }
-      })
-}
+  Marker.find({}, function (err, marker) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.status(200).send({ marker });
+    }
+  });
+};
 
 exports.sendMarkerFromHour = (req, res, next) => {
-    const myDate = new Date(Date.now())
-    const myDateStart = new Date(Date.now() - 1 * 60 * 60 * 1000)
-    const myTime = moment(myDate).format("h:mm:ss a")
-    const myTimeStart = moment(myDateStart).format("h:mm:ss a")
-    Marker.find({time: { $gte : myTimeStart, $lte :  myTime }}, function(err, marker) {
-        if (err) {
-          res.send(err);
-        } else {
-          res.status(200).send({marker});
-        }
-      })
-  }
+  const myDate = new Date(Date.now());
+  const myDateStart = new Date(Date.now() - 1 * 60 * 60 * 1000);
+  const myTime = moment(myDate).format("h:mm:ss a");
+  const myTimeStart = moment(myDateStart).format("h:mm:ss a");
+  Marker.find({ time: { $gte: myTimeStart, $lte: myTime } }, function (
+    err,
+    marker
+  ) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.status(200).send({ marker });
+    }
+  });
+};
 
 exports.postMarker = async (req, res, next) => {
-    const { lat, long, type } = req.body;
-    try {
-        const newMarker = new Marker({
-            lat, 
-            long,
-            type,
-        })
-        await newMarker.save();
-        res.status(200).send({marker: {...newMarker}})
-    } catch(err){
-        res.send('error')
-    }
-}
+  const { lat, long, type } = req.body;
+  try {
+    const newMarker = new Marker({
+      lat,
+      long,
+      type,
+    });
+    await newMarker.save();
+    res.status(200).send({ marker: { ...newMarker } });
+  } catch (err) {
+    res.send("error");
+  }
+};
--- controllers/pickup.js
+++ controllers/pickup.js
@@ -1,41 +1,66 @@
 const Pickup = require("../model/pickup.js");
 require("dotenv/config");
-const moment = require('moment');
+const moment = require("moment");
 
 exports.sendPickup = (req, res, next) => {
-    Pickup.find({}, function(err, pickup) {
-        if (err) {
-          res.send(err);
-        } else {
-          res.status(200).send({pickup});
-        }
-      })
-}
+  Pickup.find({}, function (err, pickup) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.status(200).send({ pickup });
+    }
+  });
+};
 
 exports.sendPickupFromHour = (req, res, next) => {
-  const myDate = new Date(Date.now())
-  const myDateStart = new Date(Date.now() - 1 * 60 * 60 * 1000)
-  const myTime = moment(myDate).format("h:mm:ss a")
-  const myTimeStart = moment(myDateStart).format("h:mm:ss a")
-  Pickup.find({time: { $gte : myTimeStart, $lte :  myTime }}, function(err, pickup) {
+  const myDate = new Date(Date.now());
+  const myDateStart = new Date(Date.now() - 1 * 60 * 60 * 1000);
+  const myTime = moment(myDate).format("h:mm:ss a");
+  const myTimeStart = moment(myDateStart).format("h:mm:ss a");
+  Pickup.find({ time: { $gte: myTimeStart, $lte: myTime } }, function (
+    err,
+    pickup
+  ) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.status(200).send({ pickup });
+    }
+  });
+};
+
+exports.sendPickupFromPastHours = (req, res, next) => {
+  let myDate = new Date(Date.now());
+  let myDateStart = new Date(Date.now() - 1 * 60 * 60 * 1000);
+  const myTime = moment(myDate).format("h:mm:ss a");
+  const myTimeStart = moment(myDateStart).format("h:mm:ss a");
+  const orArray = [];
+  for (let i = 1; i++; i < 5) {
+    myDate += 7 * 24 * 60 * 60 * 1000;
+    orArray.push({ day: moment(myDate).format("dddd, MMMM Do YYYY") });
+  }
+  Pickup.find(
+    { time: { $gte: myTimeStart, $lte: myTime }, $or: orArray },
+    function (err, marker) {
       if (err) {
         res.send(err);
       } else {
-        res.status(200).send({pickup});
+        res.status(200).send({ marker });
       }
-    })
-}
+    }
+  );
+};
 
 exports.postPickup = async (req, res, next) => {
-    const { lat, long } = req.body;
-    try {
-        const newPickup = new Pickup({
-            lat, 
-            long,
-        })
-        await newPickup.save();
-        res.status(200).send({pickup: {...newPickup}})
-    } catch(err){
-        res.send('error')
-    }
-}
+  const { lat, long } = req.body;
+  try {
+    const newPickup = new Pickup({
+      lat,
+      long,
+    });
+    await newPickup.save();
+    res.status(200).send({ pickup: { ...newPickup } });
+  } catch (err) {
+    res.send("error");
+  }
+};
