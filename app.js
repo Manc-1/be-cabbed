71effbe730204573eeea6da5e213ae8ce60c3669
VoltrefferNL <nielsdevisser@hotmail.com>
2020-05-22 12:21:14 +0100

    Added additional error handeling

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const apiRouter = require("./routes/apiRouter");
 
 const connection = require("./connection");
 
-const { handleCustomErrors, handle500s } = require("./errors");
+const {
+  handleCustomErrors,
+  handle500s,
+  handleInvalidPaths,
+} = require("./errors");
 
 const User = require("./model/user");
 
@@ -16,11 +20,13 @@ app.use(express.json());
 
 app.use("/api", apiRouter);
 
+app.all("/*", handleInvalidPaths);
+
 app.use(handleCustomErrors);
 app.use(handle500s);
 
 app.listen(process.env.PORT || 3000, () =>
-  console.log(`Example app listening at http://localhost:${process.env.PORT}`)
+  console.log(`App listening at http://localhost:${process.env.PORT}`)
 );
 
 module.exports = app;
