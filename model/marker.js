dcfbe0a7ade1ab81d2a36049e0ca0302579e64dc
VoltrefferNL <nielsdevisser@hotmail.com>
2020-05-24 17:57:23 +0100

    Added error handeling for bad request

--- model/marker.js
+++ model/marker.js
@@ -1,14 +1,12 @@
 const mongoose = require("mongoose");
-var moment = require('moment');
+var moment = require("moment");
 
 const Marker = new mongoose.Schema({
-  lat: Number,
-  long: Number,
-  type: String,
-  date: {type: String, default: () => moment().format("dddd, MMMM Do YYYY")},
-  time: {type: String, default: () => moment().format("h:mm:ss a")}
+  lat: { type: Number, required: true },
+  long: { type: Number, required: true },
+  type: { type: String, required: true },
+  date: { type: String, default: () => moment().format("dddd, MMMM Do YYYY") },
+  time: { type: String, default: () => moment().format("h:mm:ss a") },
 });
-
-
 
 module.exports = mongoose.model("marker", Marker);
