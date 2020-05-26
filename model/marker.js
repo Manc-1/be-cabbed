879c4b935b3ba3704571b249de97dd3a0f5fdb5a
joannakendall <58439846+joannakendall@users.noreply.github.com>
2020-05-26 09:10:20 +0100

    changed keys to latitude and longitude

--- model/marker.js
+++ model/marker.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 var moment = require('moment');
 
 const Marker = new mongoose.Schema({
-  lat: Number,
-  long: Number,
+  latitude: Number,
+  longitude: Number,
   type: String,
   date: {type: String, default: () => moment().format("dddd, MMMM Do YYYY")},
   time: {type: String, default: () => moment().format("h:mm:ss a")}
--- model/pickup.js
+++ model/pickup.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 var moment = require('moment');
 
 const Pickup = new mongoose.Schema({
-  lat: Number,
-  long: Number,
+  latitude: Number,
+  longitude: Number,
   date: {type: String, default: () => moment().format("dddd, MMMM Do YYYY")},
   time: {type: String, default: () => moment().format("h:mm:ss a")}
 });
